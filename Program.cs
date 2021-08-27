e91542a0c47d8d4e0728dabdf197a8b1026967bb
GuilhermeDSGL <72310683+Guilherme-DSGL@users.noreply.github.com>
2021-08-27 19:54:20 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+namespace hello_word {
+    class Program {
+        static void Main(string[] args) {
+            
+            var calcular = new Calculadora();
+            var y = 0;
+            var x = 0;
+            while (y == 0) {
+                Console.WriteLine("Digite um numero");
+                calcular.n1 = double.Parse(Console.ReadLine());
+                Console.WriteLine("1- fatorial 2- selecionar outro numero");
+                x = int.Parse(Console.ReadLine());
+                switch (x) {
+                    case 1:
+                        calcular.Fatorial(calcular.n1);
+                        Console.WriteLine(calcular.resultado);
+
+                        break;
+                    case 2:
+                        Console.WriteLine("Digite um numero");
+                        calcular.n2 = double.Parse(Console.ReadLine());
+                        Console.WriteLine("Selecione 1- somar, 2-subtrair, 3- multiplicar, 4- dividir");
+                        x = int.Parse(Console.ReadLine());
+                        switch (x) {
+                            case 1:
+                                calcular.somar(calcular.n1, calcular.n2);
+                                Console.WriteLine(calcular.resultado);
+                                break;
+                            case 2:
+                                calcular.subtrair(calcular.n1, calcular.n2);
+                                Console.WriteLine(calcular.resultado);
+                                break;
+                            case 3:
+                                calcular.multiplicar(calcular.n1, calcular.n2);
+                                Console.WriteLine(calcular.resultado);
+                                break;
+                            case 4:
+                                calcular.dividir(calcular.n1, calcular.n2);
+                                Console.WriteLine(calcular.resultado);
+                                break;
+                            default:
+                                break;
+                        }
+                        break;
+                        default:
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ referencias/Operacoes.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace hello_word{
+    public class Calculadora{
+        public double n1;
+        public double n2;
+        public double resultado;
+        public double resultadof = 1;
+        public double somar(double n1, double n2) {
+            resultado = n1 + n2;
+            return resultado;
+        }
+        public double subtrair(double n1, double n2) {
+            resultado = n1 - n2;
+            return resultado;
+        }
+        public double multiplicar(double n1, double n2) {
+            resultado = n1 * n2;
+            return resultado;
+        }
+        public double dividir(double n1, double n2) {
+            resultado = n1 / n2;
+            return resultado;
+        }
+        public double Fatorial(double n1) {
+            resultado = 1;
+            for (int i = 1; i<=n1; i++) {
+                resultado = resultado * i;
+            }
+            return resultado;
+        }
+    }
+}
